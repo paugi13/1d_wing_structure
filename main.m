eb59b2a7f7da7afee3c64aff90c7c6f91068088c
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-03-28 13:17:31 +0200

    Initiated process

--- main.m
+++ main.m
@@ -11,14 +11,14 @@ close all;
 %% INPUT DATA
 
 % Material properties
-E = 0;
+E = 85e9;
 
 % Cross-section parameters
-t1 = 0;
-t2 = 0;
-h1 = 0;
-h2 = 0;
-b = 0;
+t1 = 1.5e-3;
+t2 = 4e-3;
+h1 = 0.5;
+h2 = 0.25;
+b = 0.775;
 
 
 % Other data
@@ -26,8 +26,8 @@ g = 9.81;
 L1 = 5;
 L2 = 10;
 L = L1+L2;
-Me = 0;
-M = 0;
+Me = 2550;
+M = 35000;
 
 % Number of elements for each part
 nel = [3,6];
@@ -37,7 +37,22 @@ Nel = 96; % Number of elements for the "exact" solution
 
 % Compute section: 
 % A  - Section area 
+h_aux = h1/2-h2/2;
+longitud = sqrt(b^2+h_aux^2);
+A1 = t2*h1;
+A2 = t2*h2;
+A3 = 2*longitud*t1;
+A =  A1 + A2 + A3;
+
 % Iz - Section inertia
+x_cg = (A2*b + A3*b/2)/A;  
+
+angle_aux = atan((h1/2-h2/2)/b);
+
+Izz1 = 1/12*t2*h1^3;
+Izz2 = 1/12*t2*h2^3;
+Izz3 = 2*(1/12*t1*longitud^3*sin(angle_aux)^2 + A3/2*(h1/2-b/2*tan(angle_aux))^2);
+Iz = Izz1 + Izz2 + Izz3;
 
 % Compute parameter l:
 % l - Equilibrium parameter
