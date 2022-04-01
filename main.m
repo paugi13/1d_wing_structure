ae109f548087cc698ca17d6e4e6c3137e27e61dd
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-04-01 16:25:29 +0200

    Fixed plotting features

--- main.m
+++ main.m
@@ -29,18 +29,14 @@ Me = 2550;
 M = 35000;
 
 % Number of elements for each part
-nel = [3,6];
+nel = [3,6, 12, 24, 48, 96];
 
 %%  Essential parameters. 
 % Everything is built so that it works by changing Nel's value. 
 Nel = 96; % Number of elements for the "exact" solution
 n_d = 1;    %Problem dimension
-n_nod = Nel+1;  %Total number of nodes.
 n_ne = 2;   % Nodes in a beam
 n_i = 2;    % DOF per node.
-n_dof = n_nod*n_i;  %Total number of degrees of freedom.
-
-x = 0:L/Nel:L;
 
 % Fixed degrees of freedom
 % First node can't move upwards or rotate.
@@ -78,35 +74,29 @@ Iyy = (Iyy1 + A1*z_cg^2) + (Iyy2 + A2*(z_cg-b)^2) + 2*(Iyy3 + A3/2*(z_cg-b/2)^2)
 
 % Compute parameter l:
 % l - Equilibrium parameter
-% aux_M = 21875; %[kg]
-
 aux_M = 1.7863e+04;
 aux_Q = 32/3; %[N]
 l = (aux_M*g)/aux_Q;
 
+% This plot is empty at first
+fig = plotBeamsInitialize(L1+L2);
 
+for k = 1:length(nel)
+    
+ n_nod = nel(k)+1;  %Total number of nodes.
+ n_dof = n_nod*n_i;  %Total number of degrees of freedom.
+ 
  %% PREPROCESS
-    
-    % Nodal coordinates
-    %  x(a,j) = coordinate of node a in the dimension j
-    % Complete the coordinates
-    
-    % Nodal connectivities  
-    %  Tnod(e,a) = global nodal number associated to node a of element e
-    
+ x = 0:L/nel(k):L;
     % Material properties matrix
-    %  mat(m,1) = Young modulus of material m
-    %  mat(m,2) = Section area of material m
-    %  mat(m,3) = Section inertia of material m
     mat = [% Young M.        Section A.    Inertia 
               E,                A,         Iz;  % Material (1)
     ];
-
     % Material connectivities
     %  Tmat(e) = Row in mat corresponding to the material associated to element e 
     
-Tn = zeros(Nel, n_ne);
-for i = 1:Nel
+Tn = zeros(nel(k), n_ne);
+for i = 1:nel(k)
     Tn(i,:) = [i, i+1];
 end
 
@@ -120,16 +110,16 @@ end
     % Mz - Internal bending moment at each elements's nodes [nel x nne]
     
 % Compute Td: Only 2 degrees of freedom per node (deflection and rotation).
-Td = connectDOFs(Nel,n_ne,n_i,Tn);
+Td = connectDOFs(nel(k),n_ne,n_i,Tn);
 
 % Independent element stiffness matrix
-[Kel, l_e_vector] = computeKelBar(n_i,Nel,x,Tn,E, Iz);
+[Kel, l_e_vector] = computeKelBar(n_i,nel(k),x,Tn,E, Iz);
 
 % Element force vector
-F_el = computeElementForceVector(n_i, n_nod, Nel, x, l_e_vector, L1, L2, l, M);
+F_el = computeElementForceVector(n_i, n_nod, nel(k), x, l_e_vector, L1, L2, l, M);
 
 % Global matrix
-[KG, Fext] = assemblyKG(Nel,n_ne*n_i,n_dof,Td,Kel, F_el);
+[KG, Fext] = assemblyKG(nel(k),n_ne*n_i,n_dof,Td,Kel, F_el);
 
 % Apply conditions 
 [vL,vR,uR] = applyCond(n_i,n_dof,fixNod);
@@ -138,17 +128,14 @@ F_el = computeElementForceVector(n_i, n_nod, Nel, x, l_e_vector, L1, L2, l, M);
 [u,R] = solveSys(vL,vR,uR,KG,Fext);
 
 %Internal distributions
-[Fy, Mz, pu, pt] = computeInternalDistributions(l_e_vector,u, Nel, n_ne, n_i, Td, Kel);
-
-
+[Fy, Mz, pu, pt] = computeInternalDistributions(l_e_vector,u, nel(k), n_ne, n_i, Td, Kel);
 
 
 %% POSTPROCESS
 
 u_an = zeros(size(u,1)/2,1);
 theta_an = zeros(size(u,1)/2,1);
-Fy_an = zeros(size(u,1)/2,1);
-Mz_an = zeros(size(u,1)/2,1);
+
 j = 1;
 for i = 1:2:size(u,1)
     u_an(j) = u(i,1);
@@ -156,31 +143,26 @@ for i = 1:2:size(u,1)
     j = j+1;
 end
 
-
-Fy_an = zeros(Nel+1,1);
-Mz_an = zeros(Nel+1,1);
-Fy_an(2:Nel+1,1) = Fy(:,1);
-Mz_an(2:Nel+1,1) = Mz(:,1);
+Fy_an = zeros(nel(k)+1,1);
+Mz_an = zeros(nel(k)+1,1);
+Fy_an(2:nel(k)+1,1) = Fy(:,1);
+Mz_an(2:nel(k)+1,1) = Mz(:,1);
 Fy_an(1,1) = Fy(1,1);
 Mz_an(1,1) = Mz(1,1);
 
-
-% Error calculus
-%Taking into account the deflection at the wing tip
-err = (u(193,1) - u(193,1))/u(193,1);
-
-
-% Plot analytical solution
-fig = plotBeamsInitialize(L1+L2,x,u_an,theta_an,Fy_an,Mz_an);
-
-
-for k = 1:length(nel)
 % Number of subdivisions and plots
     nsub = Nel/nel(k);
     plotBeams1D(fig,x,Tn,nsub,pu,pt,Fy,Mz)
     drawnow;
+
+
 end
 
+%%  Postprocessing
+
+% Error calculus
+%Taking into account the deflection at the wing tip
+err = (u(193,1) - u(193,1))/u(193,1);
     
 %Von Mises criterion
 sig = y_max*Mz/Iz;
